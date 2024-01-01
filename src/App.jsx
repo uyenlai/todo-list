8b1842552a20809cd401dd00f37f389524b569f9
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-01 18:37:31 +0200

    main features are adding todo and deleting todo were done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,102 @@
+import { useState } from "react";
+
 export default function App() {
+  const [enteredValue, setEnteredValue] = useState({
+    title: "",
+    deadline: "",
+    status: ["done", "not started", "in progress"],
+  });
+  const [todoList, setTodoList] = useState([]);
+  const [showForm, setShowForm] = useState(true);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodoList((prevState) => {
+      return [
+        ...prevState,
+        {
+          title: enteredValue.title,
+          deadline: enteredValue.deadline,
+          id: Math.random() * 1000,
+        },
+      ];
+    });
+    setEnteredValue({
+      title: "",
+      deadline: "",
+      //status: ["done", "not started", "in progress"],
+    });
+  }
+
+  function handleValueChange(e, identifier) {
+    setEnteredValue({
+      ...enteredValue,
+      [identifier]: e.target.value,
+    });
+  }
+
+  function handleDelete(id) {
+    setTodoList((prevState) => {
+      return prevState.filter((item) => item.id !== id);
+    });
+  }
+
+  function toggleShowForm() {
+    setShowForm(false);
+  }
+
   return (
     <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <h1 className="header">New Item</h1>
-          <input type="text" />
+      {showForm && (
+        <button onClick={toggleShowForm} className="btn">
+          Add a new todo
+        </button>
+      )}
+      {!showForm && (
+        <form className="new-item-form" onSubmit={handleSubmit}>
+          <h1 className="header">Add new todo</h1>
+          <div className="form-row">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredValue.title}
+              onChange={(e) => handleValueChange(e, "title")}
+              required
+            />
+          </div>
+
+          <div className="form-row">
+            <label>Deadline</label>
+            <input
+              type="date"
+              value={enteredValue.deadline}
+              onChange={(e) => handleValueChange(e, "deadline")}
+              required
+            />
+          </div>
           <button className="btn">Add</button>
-        </div>
-      </form>
+        </form>
+      )}
       <h1 className="header">To do list</h1>
-      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Do laundry
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Clean the house
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
+      {todoList.length === 0 && <p>There is no item.</p>}
+      {todoList.length > 0 && (
+        <ul className="list">
+          {todoList.map((item) => {
+            return (
+              <li key={item.id}>
+                <p>{item.title}</p>
+                <p>{item.deadline}</p>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }
